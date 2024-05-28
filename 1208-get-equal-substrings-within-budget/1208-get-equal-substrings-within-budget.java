7fc854d5e68d585021de2ea58e53957b14814f65
LOHITH MR <lohithmr494@gmail.com>
2024-05-28 09:52:07 +0530

    Time: 5 ms (97.52%), Space: 43.7 MB (9.32%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -1,33 +1,25 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        int n =s.length();
-        int[] cost =new int[n];
-        for (int i=0;i<n;i++) 
-        {
-            cost[i]=Math.abs(s.charAt(i)-t.charAt(i));
-        }  
-        int[] prefixSums = new int[n+1];
-        for (int i=0;i<n;i++) 
-        {
-            prefixSums[i+1]=prefixSums[i]+cost[i];
-        }   
-        int maxLength=0; 
-        for (int i=0;i<=n;i++) 
-        {
-            int low=i+1,high=n+1;
-            while (low<high) 
-            {
-                int mid=low+(high-low)/2;
-                if (prefixSums[mid]-prefixSums[i]<=maxCost) 
-                {
-                    low=mid+1;
-                } 
-                else 
-                {
-                    high=mid;
-                }
+        int n = s.length();
+        int[] cost = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            cost[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        }
+        
+        int start = 0;
+        int currentCost = 0;
+        int maxLength = 0;
+        
+        for (int end = 0; end < n; end++) {
+            currentCost += cost[end];
+            
+            while (currentCost > maxCost) {
+                currentCost -= cost[start];
+                start++;
             }
-            maxLength = Math.max(maxLength,low-i-1);
+            
+            maxLength = Math.max(maxLength, end - start + 1);
         }
         
         return maxLength;
