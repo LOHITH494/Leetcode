891c90cb9d5759a27e60f6a7695d76134afed25b
LOHITH MR <lohithmr494@gmail.com>
2024-07-02 14:49:42 +0530

    Time: 1 ms (100%), Space: 46.3 MB (43.85%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    int maxLevel, maxSum;
+    public int deepestLeavesSum(TreeNode root) {
+        maxSum=0;
+        maxLevel=-1;
+        deepestLeavesSum(root,1);
+        return maxSum;
+    }
+
+    private void deepestLeavesSum(TreeNode root,int level) {
+        if(root==null) 
+        {
+            return;
+        }
+        if(level>maxLevel) 
+        {
+            maxLevel = level;
+            maxSum = 0;
+        }
+        if(level==maxLevel && root.left==null && root.right==null) 
+        {
+            maxSum += root.val; 
+        }
+        deepestLeavesSum(root.left,level+1);
+        deepestLeavesSum(root.right,level+1);
+    }
+}
