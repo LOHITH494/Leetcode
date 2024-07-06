ce09a477f519c0e4d4c908298f5769f81f38380f
LOHITH MR <lohithmr494@gmail.com>
2024-07-06 16:34:08 +0530

    Time: 6 ms (99.44%), Space: 63.6 MB (7.12%) - LeetHub

--- 3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.java
+++ 3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.java
@@ -1,10 +1,21 @@
 class Solution {
     public int minOperations(int[] nums) {
-    int flip=0;
+    int flip=0,count=0;
     for(int n:nums)
     {
-        flip+=1-n^flip&1;
+        if(n==flip)
+        {
+            count++;
+            if(flip==0)
+            {
+                flip=1;
+            }
+            else
+            {
+                flip=0;
+            }
+        }
     }    
-    return flip;
+    return count;
     }
 }
